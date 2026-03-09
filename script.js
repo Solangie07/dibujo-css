0254e92a2dcb3c71ef75e156c63f981850041abd
Solangie07 <solmillanbotero@gmail.com>
2026-03-09 15:46:05 +0000

    lista

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
 console.log("hola");
 
 const varTitulo = document.getElementById("titulo");
+const varTexto = document.getElementById("texto");
+
+const listaTextos = ["perros", "gatos", "pajaros", "tigre", "jaguar"];
+
+let estadoInicial = true; 
+let indice = 0; 
 
 varTitulo.addEventListener("click", () => {
-    varTitulo.innerText = "¡Buenos días!"; 
-    varTitulo.style.backgroundColor = "blue"; 
-    varTitulo.style.color = "white"; 
+    if(estadoInicial === true) { 
+        varTitulo.innerText = "¡Buenos días!"; 
+    varTitulo.style.backgroundColor = "white"; 
+    varTitulo.style.color = "blue"; 
+    } else {
+      varTitulo.innerText = "hola"; 
+    varTitulo.style.color = "red";
+    varTitulo.style.backgroundColor = "transparent"; 
+    }
+    estadoInicial = !estadoInicial; 
+})
+
+varTexto.addEventListener("click", () => {
+varTexto.innerText = listaTextos[indice];
+if(indice < listaTextos.length - 1){
+  indice = indice + 1;  
+} else {
+    // indice = 0; // si quiero que vuelva a empezar 
+    varTexto.innerText = "FIN.";
+}
 })
